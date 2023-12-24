87e29c374625666b6b59d00e55651f2e00de858d
Anezijane <ahnnieanezi@gmail.com>
2023-12-24 17:21:55 +0100

    add background

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/main.js",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
