9876348aa83c0414cf5c8af62002d93f4bbb8f3e
Anezijane <ahnnieanezi@gmail.com>
2023-12-27 18:23:44 +0100

    select elements

--- src/main.js
+++ src/main.js
@@ -3,12 +3,3 @@ const rating = document.getElementById('rating');
 const submit = document.getElementById('submit');
 const numSelected = document.querySelector(".num-selected");
 
-function setActive(butn) {
-    var buttons = document.getElementsByName('rating');
-    for (let i=0; i<buttons.length;i++) {
-        buttons[i].classList.remove("active")
-    }
-
-    var activeBtn = document.getElementById(butn);
-    activeBtn.classList.add('active');
-}
