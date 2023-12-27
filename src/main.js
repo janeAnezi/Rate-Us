ca4114d42913a878a6a95926d4c8ec3f6e9b39e3
Anezijane <ahnnieanezi@gmail.com>
2023-12-27 21:18:58 +0100

    add success page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-const ratingContainer = document.getElementById('ratingcontainer');
-const rating = document.getElementById('rating');
-const submit = document.getElementById('submit');
-const numSelected = document.querySelector(".num-selected");
+let selectedRating = 0;
 
+    function selectRating(rating) {
+        selectedRating = rating;
+        highlightStars(rating);
+
+        
+    }
+
+    function highlightStars(rating) {
+        const stars = document.querySelectorAll('.star');
+        stars.forEach((star, index) => {
+            if (index < rating) {
+                star.style.color = 'gold';
+            } else {
+                star.style.color = 'black';
+            }
+        });
+    }
+
+    function showRating() {
+        if (selectedRating !== 0) {
+            document.getElementById('result-container').style.display = 'block';
+            document.getElementById('rating-container').style.display = 'none';
+            document.getElementById('selected-rating').textContent = `You selected ${selectedRating} out of 5.`;
+
+        }
+        
+    }
