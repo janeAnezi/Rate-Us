1281f8c4c80e0576b4e31843ad881293c0d49f5d
Anezijane <ahnnieanezi@gmail.com>
2023-12-27 22:35:41 +0100

    add highlight rating value

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
+// Initialize a variable to store the selected rating
 let selectedRating = 0;
 
     function selectRating(rating) {
         selectedRating = rating;
-        highlightStars(rating);
-
-        
+        highlightStars(rating);   
     }
 
     function highlightStars(rating) {
+       // Get all radio button elements with the class 'rating'
         const stars = document.querySelectorAll('.star');
+       // Loop through each class to find the selected one
         stars.forEach((star, index) => {
             if (index < rating) {
                 star.style.color = 'gold';
@@ -18,12 +19,15 @@ let selectedRating = 0;
         });
     }
 
-    function showRating() {
+         // If a rating is selected, display it in the result container
+         function showRating() {
         if (selectedRating !== 0) {
+            // Show the result container
             document.getElementById('result-container').style.display = 'block';
+            // hide the content container
             document.getElementById('rating-container').style.display = 'none';
+            // Set the text content of the result paragraph
             document.getElementById('selected-rating').textContent = `You selected ${selectedRating} out of 5.`;
-
         }
-        
+
     }
