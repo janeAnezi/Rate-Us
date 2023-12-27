294e8a38f654d47fd5d639fbdadf921e6ddd74f1
Anezijane <ahnnieanezi@gmail.com>
2023-12-27 18:07:04 +0100

    add active buttons

--- src/main.js
+++ src/main.js
@@ -3,21 +3,12 @@ const rating = document.getElementById('rating');
 const submit = document.getElementById('submit');
 const numSelected = document.querySelector(".num-selected");
 
+function setActive(butn) {
+    var buttons = document.getElementsByName('rating');
+    for (let i=0; i<buttons.length;i++) {
+        buttons[i].classList.remove("active")
+    }
 
-function submitRating() {
-    // Get the selected rating value
-    var selectedRating = document.getElementById('rating').value;
-
-    // Clear existing content
-    document.getElementById('contentContainer').innerHTML = '';
-
-    // Create new content based on the selected rating
-    var newContent = document.createElement('div');
-    newContent.innerHTML = '<h2>Selected Rating: ' + selectedRating + ' stars</h2>';
-    
-    // You can customize the content based on the selected rating here
-    // For example, add different paragraphs, images, etc.
-
-    // Append the new content to the container
-    document.getElementById('contentContainer').appendChild(newContent);
+    var activeBtn = document.getElementById(butn);
+    activeBtn.classList.add('active');
 }
