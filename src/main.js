703fe83c3691577e4dcdd0b5cd40446d175f7b7a
Anezijane <ahnnieanezi@gmail.com>
2023-12-27 16:53:44 +0100

    modified html page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const ratingContainer = document.getElementById('ratingcontainer');
 const rating = document.getElementById('rating');
 const submit = document.getElementById('submit');
+const numSelected = document.querySelector(".num-selected");
+
 
 function submitRating() {
     // Get the selected rating value
